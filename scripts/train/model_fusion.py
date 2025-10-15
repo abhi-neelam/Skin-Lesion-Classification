44acc9af0a4f66a7bc552cece2f54145a40f7025
Abhinav Neelam <abhinavneelam949@gmail.com>
2025-10-14 19:32:20 -0700

    create initial version of lightgbm fusion for testing

--- scripts/train/model_fusion.py
+++ scripts/train/model_fusion.py
@@ -4,6 +4,8 @@
 import argparse
 import torch
 import csv
+import yaml
+import wandb
 from torch import nn
 from timm import utils
 from timm.data import create_dataset
@@ -16,7 +18,10 @@ from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_sc
 from sklearn.preprocessing import label_binarize
 from sklearn.manifold import TSNE
 from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+from sklearn.model_selection import StratifiedKFold, cross_val_score
 from imblearn.metrics import sensitivity_score, specificity_score
+import lightgbm as lgb
+from lightgbm import LGBMClassifier
 import matplotlib.pyplot as plt
 from collections import OrderedDict
 from datetime import datetime
@@ -24,6 +29,8 @@ from pathlib import Path
 import seaborn as sns
 import pandas as pd
 import numpy as np
+import joblib
+import random
 import time
 import os
 
@@ -31,7 +38,13 @@ def random_seed(seed=42):
     torch.manual_seed(seed)
     np.random.seed(seed)
     random.seed(seed)
-    
+
+def experiment_name(args):
+    if args.experiment:
+        return args.experiment
+    name = "+".join(args.models)[:60].replace("/", "_")
+    return "-".join([datetime.now().strftime("%Y%m%d-%H%M%S"), name, "224"])
+        
 class LightWeight_Baseline(nn.Module):
     def __init__(self, model, num_classes, pretrained=True, in_chans=3):
         super().__init__()
@@ -59,6 +72,12 @@ class LightWeight_Baseline(nn.Module):
 def parse_args():
     parser = argparse.ArgumentParser(description='PyTorch Validation')
 
+    parser.add_argument('--output', default='', type=str, metavar='PATH',
+                   help='path to output folder (default: none, current dir)', required=True)
+
+    parser.add_argument('--experiment', default='', type=str, metavar='NAME',
+                   help='name of train experiment, name of sub-folder for output', required=True)
+    
     parser.add_argument('--models', required=True, nargs='+', default='', type=str, metavar='MODEL',
                    help='Names of models to fuse (e.g: "mobilenetv3_large_100 resnet50")')
     
@@ -92,15 +111,64 @@ def parse_args():
     parser.add_argument('--data-dir', required=True, metavar='DIR',
                     help='path to dataset (root dir)')
     
-    parser.add_argument('--epochs', type=int, default=100, metavar='N',
-                   help='number of epochs to train (default: 100)')
-    
     parser.add_argument('--seed', type=int, default=42, metavar='S',
                    help='random seed (default: 42)')
     
+    parser.add_argument('--weight-decay', type=float, default=2e-5,
+                    help='weight decay (default: 2e-5)')
+    
+    parser.add_argument('--lr', type=float, default=0.1, metavar='LR',
+                   help='learning rate, overrides lr-base if set (default: 0.1)')
+    
+    parser.add_argument('--trees', type=int, default=300, metavar='T',
+                   help='number of boosted trees to fit (default: 300)')
+    
+    parser.add_argument('--max-depth', type=int, default=-1, metavar='D',
+                   help='max tree depth (default: -1)')
+    
+    parser.add_argument('--wandb-project', default=None, type=str,
+                    help='wandb project name', required=True)
+    
+    parser.add_argument('--wandb-tags', default=[], type=str, nargs='+',
+                    help='wandb tags', required=True)
+    
     args = parser.parse_args()
+    
+    args_text = yaml.safe_dump(args.__dict__, default_flow_style=False)
 
-    return args
+    return args, args_text
+
+def get_pre_logits_and_labels(models, loader, device):
+    for model in models:
+        model.eval()
+
+    features_np = []
+    targets_np = []
+    with torch.inference_mode():
+        for batch_idx, (input, target) in enumerate(loader):
+            if batch_idx > 3: # TODO - remove after
+                break
+            batch_size = input.shape[0]
+
+            input = input.to(device=device)
+            target = target.to(device=device)
+
+            fused_features = []
+            for model in models:
+                features = model.forward_head(input) # (batch_size, feature_count)
+                features = nn.functional.normalize(features, dim=1) # normalize each model feature vector
+                fused_features.append(features)
+
+            fused_features = torch.cat(fused_features, dim=1) # TODO - check vram usage. consider moving to cpu
+            features_np.append(fused_features)
+            targets_np.append(target)
+    features_np = torch.cat(features_np, dim=0).cpu().numpy()
+    targets_np = torch.cat(targets_np, dim=0).cpu().numpy()
+    
+    X = features_np
+    y = targets_np
+    
+    return X, y
 
 def train(args):
     if torch.cuda.is_available():
@@ -122,19 +190,40 @@ def train(args):
         model.to(device=device)
         models.append(model)
 
-    dataset = create_dataset(
+    dataset_train = create_dataset(
         '',
         root=args.data_dir,
-        split=args.split,
+        split='train',
         class_map='',
+        batch_size=args.batch_size,
         seed=args.seed,
-        input_img_mode='RGB',
+        input_key=None,
+        target_key=None
+    )
+    
+    dataset_eval = create_dataset(
+        '',
+        root=args.data_dir,
+        split='validation',
+        class_map='',
+        batch_size=args.batch_size,
+        seed=args.seed,
         input_key=None,
         target_key=None
     )
 
-    loader = create_loader(
-        dataset,
+    loader_train = create_loader(
+        dataset_train,
+        input_size=(3, 224, 224),
+        batch_size=args.batch_size,
+        no_aug=True,
+        num_workers=args.workers,
+        pin_memory=args.pin_mem,
+        device=device
+    )
+    
+    loader_eval = create_loader(
+        dataset_eval,
         input_size=(3, 224, 224),
         batch_size=args.batch_size,
         no_aug=True,
@@ -143,40 +232,25 @@ def train(args):
         device=device
     )
 
-    losses = utils.AverageMeter()
-    top1 = utils.AverageMeter()
-    top5 = utils.AverageMeter()
+    X_train, y_train = get_pre_logits_and_labels(models, loader_train, device)
+    X_valid, y_valid = get_pre_logits_and_labels(models, loader_eval, device)
 
-    for model in models:
-        model.eval()
-
-    features_np = []
-    targets_np = []
-    with torch.inference_mode():
-        for batch_idx, (input, target) in enumerate(loader):
-            batch_size = input.shape[0]
-
-            input = input.to(device=device)
-            target = target.to(device=device)
-
-            fused_features = []
-            for model in models:
-                features = model.forward_head(input) # (batch_size, feature_count)
-                features = nn.functional.normalize(features, dim=1) # normalize each model feature vector
-                fused_features.append(features)
-
-            fused_features = torch.cat(fused_features, dim=1)
-            features_np.append(fused_features)
-            targets_np.append(target)
-    features_np = torch.cat(features_np, dim=0).cpu().numpy()
-    targets_np = torch.cat(targets_np, dim=0).cpu().numpy()
+    eval_result = {}
     
-    df = pd.DataFrame(features_np)
-    df["target"] = targets_np
+    clf = LGBMClassifier(n_estimators=args.trees, max_depth=args.max_depth, objective='multiclass', device_type="gpu", verbosity=-1, n_jobs=args.workers, random_state=args.seed, learning_rate=args.lr, reg_lambda=args.weight_decay, early_stopping_rounds=50)
+    # TODO - check if device is correct. use gpu?
+    cv = StratifiedKFold(n_splits=5, shuffle=True, random_state=args.seed)
+    clf.fit(X_train, y_train, eval_set=[(X_valid, y_valid)], eval_metric="multi_logloss",
+        callbacks=[
+            lgb.log_evaluation(),
+            lgb.record_evaluation(eval_result),
+        ]
+    )
+    
+    predictions = clf.predict(X_valid)
+    print("Valid accuracy:", accuracy_score(y_valid, predictions))
 
-
-
-    return None
+    return clf
 
 def save_df(df, args, filename):
     p = Path(args.checkpoint)
@@ -194,29 +268,33 @@ def save_fig(args, filename):
 def main():
     sns.set_palette("bright")
 
-    args = parse_args()
+    args, args_text = parse_args()
 
     if torch.cuda.is_available():
         torch.backends.cuda.matmul.allow_tf32 = True
         torch.backends.cudnn.benchmark = True
 
-    random_seed(args.seed)
-    results = train(args)
-
-    if args.results_file:
-        write_results(args.results_file, results)
+    exp_name = experiment_name(args)
     
-def write_results(results_file, results):
-    with open(results_file, mode='w') as cf:
-        if not isinstance(results, (list, tuple)):
-            results = [results]
-        if not results:
-            return
-        dw = csv.DictWriter(cf, fieldnames=results[0].keys())
-        dw.writeheader()
-        for r in results:
-            dw.writerow(r)
-        cf.flush()
+    wandb.init(
+        project=args.wandb_project,
+        name=exp_name,
+        config=args,
+        tags=args.wandb_tags,
+    )
+
+    output_dir = utils.get_outdir(args.output if args.output else './output/train', exp_name)
+    random_seed(args.seed)
+    clf = train(args)
+    
+    outdir = Path(args.output) / exp_name
+    outdir.mkdir(parents=True, exist_ok=True) # create experiment dir
+    joblib.dump(clf, str(outdir / "lgbm.pkl"))
+
+    with open(os.path.join(output_dir, 'args.yaml'), 'w') as f:
+        f.write(args_text)
+
+    wandb.finish()
 
 if __name__ == '__main__':
     main()
