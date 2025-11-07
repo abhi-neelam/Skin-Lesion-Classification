2271fcbdcc1bd2623e80596422280c2524f1b0ef
Abhinav Neelam <abhinavneelam949@gmail.com>
2025-11-07 13:05:31 -0800

    create test folder

--- scripts/timm_data_format.py
+++ scripts/timm_data_format.py
@@ -3,21 +3,24 @@ import pandas as pd
 import numpy as np
 import shutil
 
-num_classes = 8
-
-data_folder_path = "../data/ISIC_2019_Training_Input"
-annotations_file = "../data/ISIC_2019_Training_GroundTruth.csv"
+train_data_folder_path = "../data/ISIC_2019_Training_Input"
+train_annotations_file = "../data/ISIC_2019_Training_GroundTruth.csv"
+test_data_folder_path = "../data/ISIC_2019_Test_Input"
+test_annotations_file = "../data/ISIC_2019_Test_GroundTruth.csv"
 out_dir = "../data/ISIC_2019_timm"
 
 os.makedirs(out_dir, exist_ok=True) # create the output dir
 
-img_labels = pd.read_csv(annotations_file)
+train_img_labels = pd.read_csv(train_annotations_file)
+test_img_labels = pd.read_csv(test_annotations_file)
 
-train_samples = img_labels.sample(frac=0.80, random_state=42) # randomstate for consistency across objects
-valid_samples = img_labels.drop(train_samples.index)
+train_samples = train_img_labels.sample(frac=0.80, random_state=42) # randomstate for consistency across objects
+valid_samples = train_img_labels.drop(train_samples.index)
+test_samples = test_img_labels
 
-for split, samples_df in zip(["train", "validation"], [train_samples, valid_samples]):
-    samples_df.drop("UNK", axis=1, inplace=True) # remove unknown category
+for split, samples_df, data_folder_path in zip(["train", "validation", "test"], [train_samples, valid_samples, test_samples], [train_data_folder_path, train_data_folder_path, test_data_folder_path]):
+    samples_df = samples_df[samples_df['UNK'] == 0] # filter out unknown as we don't predict that category
+    samples_df.drop(["UNK", "score_weight", "validation_weight"], axis=1, inplace=True, errors='ignore') # remove some unnecessary columns
     samples_df.reset_index(drop=True, inplace=True) # reset index for stability
     ohe_labels = samples_df.iloc[:, 1:]
 
